425a2dfd30a71ee11926bb8e1d687479552ef53e
Ensar <ensarhamzic01@gmail.com>
2022-08-10 20:01:32 +0200

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Card } from "react-bootstrap";
+import { authActions } from "../store/auth-slice";
+import LoginForm from "../components/LoginForm";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const [loginError, setLoginError] = useState(null);
+  const loginUserHandler = async (user) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/login`, user);
+      const { token } = response.data;
+      console.log(token);
+      setLoginError(null);
+      dispatch(authActions.login({ token }));
+    } catch (error) {
+      setLoginError(error.response.data.message);
+    }
+  };
+  return (
+    <Card className="mt-5 m-auto" style={{ width: "40%" }}>
+      <Card.Body>
+        <Card.Title>Login to your account</Card.Title>
+        <LoginForm onFormSubmit={loginUserHandler} loginError={loginError} />
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Login;
