00c1c598c0c09975a10d02d15d11b94330950279
Ensar <ensarhamzic01@gmail.com>
2022-08-10 20:01:41 +0200

    updated page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,16 +2,20 @@ import Card from "react-bootstrap/Card";
 import RegisterForm from "../components/RegisterForm";
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const Register = () => {
+  const dispatch = useDispatch();
   const [registerError, setRegisterError] = useState(null);
   const registerUserHandler = async (user) => {
     try {
       const response = await axios.post(`${API_URL}/users/register`, user);
-      console.log(response.data);
+      const { token } = response.data;
       setRegisterError(null);
+      dispatch(authActions.login({ token }));
     } catch (error) {
       setRegisterError(error.response.data.message);
     }
