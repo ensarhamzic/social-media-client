12ee960754318624b5d5bbcce588e5478d26ad3b
Ensar <ensarhamzic01@gmail.com>
2022-08-11 22:51:14 +0200

    renders profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Posts from "../components/Posts";
 
-const Profile = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {}, []);
-  return <div>Profile</div>;
+const Profile = ({ forAuthUser }) => {
+  let userId = useSelector((state) => state.auth.user.id);
+  let { id: userIdParam } = useParams();
+  if (!forAuthUser) userId = userIdParam;
+
+  return (
+    <div>
+      <Posts userId={userId} />
+    </div>
+  );
 };
 
 export default Profile;
