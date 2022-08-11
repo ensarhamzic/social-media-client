22ac8d0732682e0774a12d2d8c97a90834f50922
Ensar <ensarhamzic01@gmail.com>
2022-08-11 21:49:46 +0200

    posts state initialized

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Profile = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {}, []);
   return <div>Profile</div>;
 };
 
