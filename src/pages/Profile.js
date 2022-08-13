9ee9a62012d4ff483e9367b40a094fcfc5fb0d5b
Ensar <ensarhamzic01@gmail.com>
2022-08-13 19:06:48 +0200

    fetches all profile data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Posts from "../components/Posts";
+import axios from "axios";
+import UserDetails from "../components/UserDetails";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const Profile = ({ forAuthUser }) => {
   let userId = useSelector((state) => state.auth.user.id);
   let { id: userIdParam } = useParams();
   if (!forAuthUser) userId = userIdParam;
 
+  const token = useSelector((state) => state.auth.token);
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/users/${userId}/posts`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setPosts(
+          response.data.posts.sort((a, b) => {
+            return new Date(b.date) - new Date(a.date);
+          })
+        );
+        setUser({
+          id: response.data.id,
+          username: response.data.username,
+          email: response.data.email,
+          firstName: response.data.firstName,
+          lastName: response.data.lastName,
+          pictureUrl: response.data.pictureUrl,
+          followers: response.data.followers,
+          following: response.data.following,
+        });
+        setError(null);
+      } catch (error) {
+        setError("Something went wrong");
+        setPosts([]);
+      }
+    };
+    getPosts();
+  }, [userId, token]);
+
+  const addPostHandler = (newPost) => {
+    setPosts((prevPosts) => [newPost, ...prevPosts]);
+  };
+
   return (
     <div>
-      <Posts userId={userId} />
+      {error && (
+        <p className="text-center text-muted mt-5" style={{ fontSize: "2rem" }}>
+          {error}
+        </p>
+      )}
+      {!error && user && <UserDetails user={user} />}
+      {!error && user && (
+        <Posts user={user} posts={posts} onAddPost={addPostHandler} />
+      )}
     </div>
   );
 };
