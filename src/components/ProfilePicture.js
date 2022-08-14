5007353deca73f4f7046b8f7a8629a12d5a300d2
Ensar <ensarhamzic01@gmail.com>
2022-08-14 11:10:23 +0200

    profile picture

---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultProfilePicture from "../images/default_profile_picture.jpg";
+
+const ProfilePicture = ({ pictureUrl, width }) => {
+  return (
+    <img
+      style={{ borderRadius: "50%", width: `${width}px` }}
+      src={pictureUrl ?? DefaultProfilePicture}
+      alt="profile"
+    />
+  );
+};
+
+export default ProfilePicture;
