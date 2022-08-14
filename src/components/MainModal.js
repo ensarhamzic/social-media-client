dc25db0949b219ae706b1f1d0d833c38bc34a33c
Ensar <ensarhamzic01@gmail.com>
2022-08-14 11:09:57 +0200

    modal

---
+++ src/components/MainModal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+const MainModal = ({ show, onHide, title, children }) => {
+  return (
+    <Modal show={show} onHide={onHide} size="lg" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default MainModal;
