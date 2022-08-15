7ce7a397a43cbd2b5ec30caae823295d63a1a66a
Ensar <ensarhamzic01@gmail.com>
2022-08-15 10:51:35 +0200

    uses modal state

--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { useSelector, useDispatch } from "react-redux";
+import { modalActions } from "../store/modal-slice";
 
-const MainModal = ({ show, onHide, title, children }) => {
+const MainModal = ({ children }) => {
+  const dispatch = useDispatch();
+  const modalHideHandler = () => {
+    dispatch(modalActions.hide());
+  };
+  const modal = useSelector((state) => state.modal);
   return (
-    <Modal show={show} onHide={onHide} size="lg" centered>
+    <Modal show={modal.show} onHide={modalHideHandler} size="lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">{title}</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {modal.title}
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>{children}</Modal.Body>
       <Modal.Footer>
-        <Button onClick={onHide}>Close</Button>
+        <Button onClick={modalHideHandler}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
