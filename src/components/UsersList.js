142f0317c79dc494308905ad855cfa1d48bef0f9
Ensar <ensarhamzic01@gmail.com>
2022-08-15 10:52:27 +0200

    fallback on empty users array

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,9 +4,18 @@ import User from "./User";
 const UsersList = ({ users }) => {
   return (
     <>
-      {users.map((u) => (
-        <User key={u.id} user={u} />
-      ))}
+      {users.length > 0 && users.map((u) => <User key={u.id} user={u} />)}
+      {users.length === 0 && (
+        <p
+          style={{
+            fontSize: "1.2rem",
+            textAlign: "center",
+            fontWeight: "bold",
+          }}
+        >
+          No users
+        </p>
+      )}
     </>
   );
 };
