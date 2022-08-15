8ac47aee798b51d5266689cc5c913081541ca888
Ensar <ensarhamzic01@gmail.com>
2022-08-15 10:52:43 +0200

    removed saving element as non seializable data

--- src/store/modal-slice.js
+++ src/store/modal-slice.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   show: false,
   title: null,
-  element: null,
 };
 
 const modalSlice = createSlice({
@@ -13,12 +12,10 @@ const modalSlice = createSlice({
     show: (state, action) => {
       state.show = true;
       state.title = action.payload.title;
-      state.element = action.payload.element;
     },
     hide: (state, action) => {
       state.show = initialState.show;
       state.title = initialState.title;
-      state.element = initialState.element;
     },
   },
 });
