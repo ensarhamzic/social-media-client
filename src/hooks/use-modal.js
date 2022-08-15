d31b3ce5e65ebbcd6e13a13013e6bd3dbc19e03d
Ensar <ensarhamzic01@gmail.com>
2022-08-15 23:07:56 +0200

    custom hook to manage modal

---
+++ src/hooks/use-modal.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useModal = () => {
+  const [show, setShow] = useState(false);
+  const [title, setTitle] = useState(null);
+  const showModal = (modalTitle) => {
+    setTitle(modalTitle);
+    setShow(true);
+  };
+  const hideModal = () => {
+    setShow(false);
+  };
+
+  return [show, showModal, hideModal, title];
+};
+
+export default useModal;
