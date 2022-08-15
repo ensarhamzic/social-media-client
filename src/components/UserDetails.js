9efdcd49ea5fd64b51905e6a1d2d419e9bcf2cd7
Ensar <ensarhamzic01@gmail.com>
2022-08-15 12:31:43 +0200

    follow/unfollow user

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import ProfilePicture from "./ProfilePicture";
-import { useDispatch } from "react-redux/es/exports";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import { modalActions } from "../store/modal-slice";
 import UsersList from "./UsersList";
 import MainModal from "./MainModal";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
-const UserDetails = ({ user }) => {
-  const [userData, setUserData] = useState([]);
+const API_URL = process.env.REACT_APP_API_URL;
+
+const UserDetails = ({ user, onFollowUnfollow }) => {
+  const [usersListdata, setUsersListData] = useState([]);
+  const authUserId = useSelector((state) => state.auth.user.id);
+  const token = useSelector((state) => state.auth.token);
   const dispatch = useDispatch();
   const followersClickHandler = () => {
-    setUserData(user.followers);
+    setUsersListData(user.followers);
     dispatch(
       modalActions.show({
         title: `${user.username}'s followers`,
@@ -18,7 +24,7 @@ const UserDetails = ({ user }) => {
     );
   };
   const followingClickHandler = () => {
-    setUserData(user.following);
+    setUsersListData(user.following);
     dispatch(
       modalActions.show({
         title: `${user.username} following`,
@@ -26,10 +32,32 @@ const UserDetails = ({ user }) => {
     );
   };
 
+  const followUnfollowUserHandler = async () => {
+    try {
+      const response = await axios.post(
+        `${API_URL}/users/${user.id}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      onFollowUnfollow(response.data);
+    } catch (error) {}
+  };
+
+  const isUserFollowing = user.followers.some((f) => f.id === authUserId);
+  const followButton = (
+    <Button
+      onClick={followUnfollowUserHandler}
+      variant={`${isUserFollowing ? "light" : "primary"}`}
+    >{`${isUserFollowing ? "Unfollow" : "Follow"}`}</Button>
+  );
   return (
     <>
       <MainModal>
-        <UsersList users={userData} />
+        <UsersList users={usersListdata} />
       </MainModal>
       <Card className="mt-5 w-75 m-auto">
         <Card.Body className="d-lg-flex m-4">
@@ -64,6 +92,7 @@ const UserDetails = ({ user }) => {
                 <span className="fw-bold">{user.following.length}</span>
               </p>
             </div>
+            {authUserId !== user.id && followButton}
           </div>
         </Card.Body>
       </Card>
