dc2db8d71b8ea8e324ae17e737635fb7aa54e2e5
Ensar <ensarhamzic01@gmail.com>
2022-08-15 23:04:30 +0200

    removed modal state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
-import modalReducer from "./modal-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    modal: modalReducer,
   },
 });
 
