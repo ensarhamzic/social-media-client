bf568ba41ce7389769de916ab05c99a805fb1918
Ensar <ensarhamzic01@gmail.com>
2022-08-17 21:17:13 +0200

    custom axios hook to send requests

---
+++ src/hooks/use-axios.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import axios from "axios"
+
+const API_URL = process.env.REACT_APP_API_URL
+
+const useAxios = () => {
+  const token = useSelector((state) => state.auth.token)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const sendRequest = async ({ url, method, data, auth, errorMessage }) => {
+    setIsLoading(true)
+    let axiosConfig = {
+      url: `${API_URL}${url}`,
+      method,
+    }
+    if (data) axiosConfig.data = data
+    if (auth)
+      axiosConfig.headers = {
+        Authentication: `Bearer ${token}`,
+      }
+
+    let response = null
+    try {
+      const serverResponse = await axios(axiosConfig)
+      response = serverResponse
+    } catch (error) {
+      setError(errorMessage ?? error.response.data.message)
+    }
+    setIsLoading(false)
+    return response
+  }
+
+  return { isLoading, error, sendRequest }
+}
+
+export default useAxios
