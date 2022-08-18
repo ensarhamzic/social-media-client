5a3ee0113fd03082f98b85cab98322817ac9a240
Ensar <ensarhamzic01@gmail.com>
2022-08-18 17:31:31 +0200

    improved

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -1,31 +1,28 @@
 import { useState, useCallback } from "react"
-import { useSelector } from "react-redux"
 import axios from "axios"
 
 const API_URL = process.env.REACT_APP_API_URL
 
 const useAxios = () => {
-  const token = useSelector((state) => state.auth.token)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
   const sendRequest = useCallback(
-    async ({ url, method, data, auth, userToken, errorMessage }) => {
+    async ({ url, method, data, token, errorMessage }) => {
       setIsLoading(true)
       let axiosConfig = {
         url: `${API_URL}${url}`,
         method,
       }
       if (data) axiosConfig.data = data
-      if (auth)
+      if (token)
         axiosConfig.headers = {
-          Authorization: `Bearer ${userToken ? userToken : token}`,
+          Authorization: `Bearer ${token}`,
         }
       let response = null
       try {
         const serverResponse = await axios(axiosConfig)
         setError(null)
-        console.log(url)
         response = serverResponse
       } catch (error) {
         setError(errorMessage ?? error.response.data.message)
@@ -33,7 +30,7 @@ const useAxios = () => {
       setIsLoading(false)
       return response
     },
-    [token]
+    []
   )
 
   return { isLoading, error, sendRequest }
