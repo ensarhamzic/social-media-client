5ce7b52f1d81797a5cf84fd2a5d78867d284f8ce
Ensar <ensarhamzic01@gmail.com>
2022-08-19 17:59:07 +0200

    renders posts which gets as prop

---
+++ src/components/PostsList.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Post from "./Post"
+
+const PostsList = ({
+  posts,
+  onPostLike,
+  onPostDelete,
+  onSubmitComment,
+  onCommentDelete,
+}) => {
+  const postDeleteHandler = async (postId) => {
+    onPostDelete(postId)
+  }
+
+  const postLikeHandler = (postId) => {
+    onPostLike(postId)
+  }
+
+  const commentSubmitHandler = (postId, commentText) => {
+    onSubmitComment(postId, commentText)
+  }
+
+  const commentDeleteHandler = (postId, commentId) => {
+    onCommentDelete(postId, commentId)
+  }
+
+  return (
+    <>
+      {posts.length > 0 &&
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            text={post.text}
+            userId={post.user.id}
+            username={post.user.username}
+            comments={post.comments}
+            likes={post.likes}
+            onPostLike={postLikeHandler}
+            onPostDelete={postDeleteHandler}
+            onCommentSubmit={commentSubmitHandler}
+            onCommentDelete={commentDeleteHandler}
+          />
+        ))}
+
+      {posts.length === 0 && (
+        <p
+          className="fw-bold"
+          style={{ fontSize: "1.2rem", textAlign: "center" }}
+        >{`This user has no posts!`}</p>
+      )}
+    </>
+  )
+}
+
+export default PostsList
