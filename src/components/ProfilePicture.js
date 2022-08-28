b17c6b19cbf6f9d841956539b4ac53a0b80b0a2e
Ensar <ensarhamzic01@gmail.com>
2022-08-28 22:00:55 +0200

    fixed bug

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import DefaultProfilePicture from "../images/default_profile_picture.jpg"
 
 const ProfilePicture = ({ pictureURL, width }) => {
+  const [imageError, setImageError] = useState(false)
+
+  const imageLoadingErrorHandler = () => {
+    setImageError(true)
+  }
+
+  let imageSrc = DefaultProfilePicture
+  if (!imageError && pictureURL) {
+    imageSrc = pictureURL
+  }
   return (
     <img
       style={{ borderRadius: "50%", width: `${width}px`, height: `${width}px` }}
-      src={pictureURL ?? DefaultProfilePicture}
+      src={imageSrc}
+      onError={imageLoadingErrorHandler}
       alt="profile"
     />
   )
